2665289b3e85d98dc37eae11fc00cb78df59fa52
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-11-14 19:57:23 +0200

    add

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 400px;
+  padding: 10px;
+`;
+
+export const Wrapper = styled.div`
+  font-size: 20px;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const SubTitle = styled.h2`
+  text-align: center;
+`;
---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  padding: 12px;
+  background-color: rgba(0, 0, 0, 0.6);
+  border: 3px solid #030303;
+  border-radius: 4px;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+  color: #fbf7f7;
+  font-size: 20px;
+`;
+
+export const Input = styled.input`
+  padding: 8px 12px;
+  font: inherit;
+  cursor: pointer;
+`;
+
+export const Button = styled.button`
+  padding: 8px 12px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 3px solid #030303;
+  color: #030303;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
